b78ca9d9414b723c1c81b5b7adad88a7b2c0acb1
kubajaro <kuba.jarooosz@gmail.com>
2022-08-08 11:48:23 +0200

    flyway and JDBC, data migration, native query

--- src/main/java/com/example/todoapp/model/SqlTaskRepository.java
+++ src/main/java/com/example/todoapp/model/SqlTaskRepository.java
@@ -1,11 +1,13 @@
 package com.example.todoapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
 
 @Repository
 public interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
+
+    @Override
+    @Query(nativeQuery = true, value = "selec count(*) > 0 from tasks where =id?1")
+    boolean existsById(Integer id);
 }
